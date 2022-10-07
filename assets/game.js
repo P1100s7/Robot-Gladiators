6802b32435cf4baa754879d57f611b4a816508fb
P1100s7 <mmanp277@gmail.com>
2022-10-07 18:05:07 -0400

    totaly broken

--- assets/game.js
+++ assets/game.js
@@ -1,24 +1,28 @@
+// var playerName = 'fuck'
 var playerName = window.prompt("what is your robots name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
+
 //you can also log multi values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyName = ["Roborto", "Amy Android", "Fuck"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var Fight = function () {
+console.log(enemyName.length);
+
+for(var i = 0; i < enemyName.length; i++) {
+  console.log(enemyName[i]);
+}
+// fight function
+var Fight = function (enemyName) {
   //alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-
-  // note the lack of quotation marks around playerName
-
-  if (playerName) {
-    playerOne.robotName = playerName;
-    console.log(playerOne.attack(enemy));
-  }
+ 
+  // ask player choses to fight, fight
+  var promptFight = window.prompt('would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   //if player chose to fight, then fight
   if (promptFight === "fight" || promptFight === "Fight") {
@@ -69,8 +73,17 @@ var Fight = function () {
     playerMoney = playerMoney - 2;
   }
   //if no (false), ask question again by running fight() again
-   else {
-    Fight();
-  }
-      
+else {
+  Fight();
+}
+// if player did not chose 1 or 2 in prompt
+ }; 
+ else {
+  window.alert("You need to pick a valid option. Try again!");
+ }
+}; // end of fight function
+
+// run fight function to start game
+for(var i = 0; i < enemyName.length; i++) {
+  Fight(enemyName[i]);
 };
